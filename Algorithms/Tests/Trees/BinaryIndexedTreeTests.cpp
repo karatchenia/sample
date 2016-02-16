70cf5ffda48463acb681282cd939761f06d91a98
karatchenia <karatchenia@mail.ru>
2016-02-16 22:05:34 +0300

    anonymous namespace

--- Algorithms/Tests/Trees/BinaryIndexedTreeTests.cpp
+++ Algorithms/Tests/Trees/BinaryIndexedTreeTests.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <tuple>
 #include "../BinaryIndexedTree.h"
 #include "../../Assert.h"
@@ -9,171 +9,126 @@ using namespace std;
 using namespace MyCompany::Algorithms::Trees;
 using namespace MyCompany::Algorithms;
 
-namespace MyCompany
+namespace
 {
-	namespace Algorithms
-	{
-		namespace Trees
-		{
-			namespace Tests
-			{
-				class BinaryIndexedTreeTest final
-				{
-					BinaryIndexedTreeTest() = delete;
+  using Number = short;
+  using Tree = BinaryIndexedTree<Number>;
 
-				public:
+  //leftIndex, rightIndex, sum.
+  using Request = tuple<size_t, size_t, Number>;
+  using Request_vector = vector<Request>;
 
-					static void Test();
+  Tree CreateTree()
+  {
+    const vector<size_t> indexesToAdd{ 1, 2, 4, 1, 7, 3, 12 };
+    const auto maxIndex = *max_element(indexesToAdd.cbegin(), indexesToAdd.cend());
 
-				private:
+    Tree result(maxIndex);
+    for (const auto& index : indexesToAdd)
+    {
+      result.add(index);
+    }
 
-					using Number = short;
-					using Tree = BinaryIndexedTree<Number>;
+    return result;
+  }
 
-					//leftIndex, rightIndex, sum.
-					using Request = tuple<size_t, size_t, Number>;
-					using Request_vector = vector<Request>;
+  void CheckIndexOutOfRange(const Tree& tree)
+  {
+    const auto maxIndex = tree.max_index();
+    const auto tooLargeIndex = 123456789;
+    const string expectedMessage = "The index (123456789) must be between 1 and "
+      + to_string(maxIndex) + ".";
+    const string message = "CheckIndexOutOfRange";
 
-					static Tree CreateTree();
+    Assert::ExpectException<out_of_range>(
+      [&](void) -> void { tree.get(tooLargeIndex, tooLargeIndex); },
+      expectedMessage, message);
+  }
 
-					static void CheckIndexOutOfRange(const Tree& tree);
+  Request_vector GetRequests1()
+  {
+    //11, 2, 3, 4, 7, 12
+    const Request_vector result{
+      Request{ 0, 1, 2 }, Request{ 1, 1, 2 },
+      Request{ 1, 12, 7 },
+      Request{ 2, 3, 2 }, Request{ 2, 6, 3 },
+      Request{ 5, 5, 0 }, Request{ 5, 6, 0 }, Request{ 6, 6, 0 },
+      Request{ 7, 7, 1 },
+    };
+    return result;
+  }
 
-					static Request_vector GetRequests1();
+  void CheckRequest(const Tree& tree, const Request& request,
+    const string& stepName)
+  {
+    const auto left = get<0>(request);
+    const auto right = get<1>(request);
+    const auto actual = tree.get(left, right);
 
-					static void CheckRequests(
-						const Tree& tree, const Request_vector& requests, const string& stepName);
+    const string separator = ", ";
+    const auto name = to_string(left) + separator + to_string(right) + separator + stepName;
 
-					static void CheckRequest(const Tree& tree, const Request& request, const string& stepName);
+    Assert::AreEqual(get<2>(request), actual, name);
+  }
 
-					static void MakeChanges(Tree& tree);
+  void CheckRequests(
+    const Tree& tree, const Request_vector& requests, const string& stepName)
+  {
+    Assert::NotEmpty(requests, stepName);
 
-					static Request_vector GetRequests2();
+    for (const auto& request : requests)
+    {
+      CheckRequest(tree, request, stepName);
+    }
+  }
 
-					static void TestValueAt(const Tree& tree, const string& stepName);
-				};
+  void MakeChanges(Tree& tree)
+  {
+    tree.add(1, 30);
+    tree.add(3, 500);
+    tree.add(6, 7000);
+  }
 
-				BinaryIndexedTreeTest::Tree BinaryIndexedTreeTest::CreateTree()
-				{
-					const vector<size_t> indexesToAdd{ 1, 2, 4, 1, 7, 3, 12 };
-					const auto maxIndex = * max_element(indexesToAdd.cbegin(), indexesToAdd.cend());
+  Request_vector GetRequests2()
+  {
+    const Request_vector result{
+      Request{ 0, 1, 32 }, Request{ 1, 1, 32 },
+      Request{ 1, 12, 7537 },
+      Request{ 2, 3, 502 }, Request{ 2, 6, 7503 },
+      Request{ 5, 5, 0 }, Request{ 5, 6, 7000 }, Request{ 6, 6, 7000 },
+      Request{ 7, 7, 1 },
+    };
+    return result;
+  }
 
-					Tree result(maxIndex);
-					for (const auto& index : indexesToAdd)
-					{
-						result.add(index);
-					}
+  void TestValueAt(const Tree& tree, const string& stepName)
+  {
+    const auto max_index = tree.max_index();
+    Assert::Greater(max_index, BinaryIndexedTree<Number>::InitialIndex, "max_index");
 
-					return result;
-				}
+    const auto prefix = stepName + "_at_";
 
-				void BinaryIndexedTreeTest::CheckIndexOutOfRange(
-					const Tree& tree)
-				{
-					const auto maxIndex = tree.max_index();
-					const auto tooLargeIndex = 123456789;
-					const string expectedMessage = "The index (123456789) must be between 1 and "
-						+ to_string(maxIndex) + ".";
-					const string message = "CheckIndexOutOfRange";
+    for (size_t i = BinaryIndexedTree<Number>::InitialIndex; i <= max_index; ++i)
+    {
+      const auto expected = tree.value_at(i);
+      const auto actual = tree.get(i, i);
+      const auto name = prefix + to_string(i);
+      Assert::AreEqual(expected, actual, name);
+    }
+  }
+}
 
-					Assert::ExpectException<out_of_range>(
-						[&](void) -> void { tree.get(tooLargeIndex, tooLargeIndex); },
-						expectedMessage, message);
-				}
-
-				BinaryIndexedTreeTest::Request_vector BinaryIndexedTreeTest::GetRequests1()
-				{
-					//11, 2, 3, 4, 7, 12
-					const Request_vector result{
-						Request{ 0, 1, 2 }, Request{ 1, 1, 2 },
-						Request{ 1, 12, 7 },
-						Request{ 2, 3, 2 }, Request{ 2, 6, 3 },
-						Request{ 5, 5, 0 }, Request{ 5, 6, 0 }, Request{ 6, 6, 0 },
-						Request{ 7, 7, 1 },
-					};
-					return result;
-				}
-
-				void BinaryIndexedTreeTest::CheckRequests(
-					const Tree& tree, const Request_vector& requests, const string& stepName)
-				{
-					Assert::NotEmpty(requests, stepName);
-
-					for (const auto& request : requests)
-					{
-						CheckRequest(tree, request, stepName);
-					}
-
-					TestValueAt(tree, stepName);
-				}
-
-				void BinaryIndexedTreeTest::CheckRequest(
-					const Tree& tree, const Request& request, const string& stepName)
-				{
-					const auto left = get<0>(request);
-					const auto right = get<1>(request);
-					const auto actual = tree.get(left, right);
-
-					const string separator = ", ";
-					const auto name = to_string(left) + separator + to_string(right) + separator + stepName;
-
-					Assert::AreEqual(get<2>(request), actual, name);
-				}
-
-				void BinaryIndexedTreeTest::MakeChanges(Tree& tree)
-				{
-					tree.add(1, 30);
-					tree.add(3, 500);
-					tree.add(6, 7000);
-				}
-
-				BinaryIndexedTreeTest::Request_vector BinaryIndexedTreeTest::GetRequests2()
-				{
-					const Request_vector result{
-						Request{ 0, 1, 32 }, Request{ 1, 1, 32 },
-						Request{ 1, 12, 7537 },
-						Request{ 2, 3, 502 }, Request{ 2, 6, 7503 },
-						Request{ 5, 5, 0 }, Request{ 5, 6, 7000 }, Request{ 6, 6, 7000 },
-						Request{ 7, 7, 1 },
-					};
-					return result;
-				}
-
-				void BinaryIndexedTreeTest::TestValueAt(const Tree& tree, const string& stepName)
-				{
-					const auto max_index = tree.max_index();
-					Assert::Greater(max_index, BinaryIndexedTree<Number>::InitialIndex, "max_index");
-
-					const auto prefix = stepName + "_at_";
-
-					for (size_t i = BinaryIndexedTree<Number>::InitialIndex; i <= max_index; ++i)
-					{
-						const auto expected = tree.value_at(i);
-						const auto actual = tree.get(i, i);
-						const auto name = prefix + to_string(i);
-						Assert::AreEqual(expected, actual, name);
-					}
-				}
-
-				void BinaryIndexedTreeTest::Test()
-				{
-					auto tree = CreateTree();
-					CheckIndexOutOfRange(tree);
-					{
-						const auto requests = GetRequests1();
-						CheckRequests(tree, requests, "step1");
-					}
-					MakeChanges(tree);
-					{
-						const auto requests = GetRequests2();
-						CheckRequests(tree, requests, "step2");
-					}
-				}
-
-				void BinaryIndexedTreeTests(void)
-				{
-					BinaryIndexedTreeTest::Test();
-				}
-			}
-		}
-	}
+void MyCompany::Algorithms::Trees::Tests::BinaryIndexedTreeTests(void)
+{
+  auto tree = CreateTree();
+  CheckIndexOutOfRange(tree);
+  {
+    const auto requests = GetRequests1();
+    CheckRequests(tree, requests, "step1");
+  }
+  MakeChanges(tree);
+  {
+    const auto requests = GetRequests2();
+    CheckRequests(tree, requests, "step2");
+  }
 }
