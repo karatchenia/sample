5ae4dcd3efbc11400026ed3acf8b08b2eecb71c5
karatchenia <karatchenia@mail.ru>
2016-02-16 22:02:58 +0300

    anonymous namespace

--- Algorithms/Tests/Numbers/LongestIncreasingSubsequenceTests.cpp
+++ Algorithms/Tests/Numbers/LongestIncreasingSubsequenceTests.cpp
@@ -1,108 +1,78 @@
 #include "LongestIncreasingSubsequence.h"
 #include "LongestIncreasingSubsequenceTests.h"
-#include "../PrintVectorUtilities.h"
+#include "../PrintUtilities.h"
 #include "../Tests/TestUtilities.h"
 
 using namespace std;
 using namespace MyCompany::Algorithms::Numbers;
 using namespace MyCompany::Algorithms;
 
-namespace MyCompany
+namespace
 {
-  namespace Algorithms
+  using Number = int;
+  using TContainer = vector<Number>;
+  using Indexes = LongestIncreasingSubsequence<Number>::Indexes;
+
+  class TestCase final : public BaseTestCase
   {
-    namespace Numbers
+    TContainer _Data;
+    Indexes _Expected;
+
+  public:
+
+    TestCase(
+      string&& name,
+      TContainer&& data,
+      Indexes&& expected)
+      : BaseTestCase(forward<string>(name)),
+      _Data(forward<TContainer>(data)), _Expected(forward<Indexes>(expected))
     {
-      namespace Tests
-      {
-        struct LongestIncreasingSubsequenceTest final
-        {
-          static void Test();
+    }
 
-        private:
+    inline const TContainer& get_Data() const { return _Data; }
+    inline const Indexes& get_Expected() const { return _Expected; }
 
-          using Number = int;
-          using TContainer = vector<Number>;
-          using Indexes = LongestIncreasingSubsequence<Number>::Indexes;
+    void Print(ostream& str) const override
+    {
+      BaseTestCase::Print(str);
 
-          class TestCase final : public BaseTestCase
-          {
-            TContainer _Data;
-            Indexes _Expected;
+      AppendSeparator(str);
+      ::Print("Data", _Data, str);
+      ::Print("Expected", _Expected, str);
+    }
+  };
 
-          public:
+  void GenerateTestCases(
+    vector<TestCase>& testCases)
+  {
+    testCases.push_back({ "Increasing",{ 5, 10, 20, 500 },{ 0, 1, 2, 3 } });
+    testCases.push_back({ "NonIncreasing",{ 50, 10 },{ 1 } });
+    testCases.push_back({ "Trivial",{ -97864 },{ 0 } });
+    testCases.push_back({ "Many",{ -50, 10, 90, 25, -3, 1000, 50, 500 },{ 0, 1, 3, 6, 7 } });
+  }
 
-            TestCase(
-              string&& name,
-              TContainer&& data,
-              Indexes&& expected);
+  void RunTestCase(const TestCase& testCase)
+  {
+    using TComparer = less<Number>;
+    using Alg = Indexes(*)(const TContainer& source, TComparer);
+    using NameAlg = pair<string, Alg>;
 
-            inline const TContainer& get_Data() const { return _Data; }
-            inline const Indexes& get_Expected() const { return _Expected; }
+    const vector<NameAlg> subTests{
+      { "Slow",LongestIncreasingSubsequence<Number>::Slow },
+      { "Fast",LongestIncreasingSubsequence<Number>::Fast },
+    };
 
-            void Print(ostream& str) const override;
-          };
-
-          static void GenerateTestCases(vector<TestCase>& testCases);
-
-          static void RunTestCase(const TestCase& testCase);
-        };
-
-        void LongestIncreasingSubsequenceTest::GenerateTestCases(
-          vector<TestCase>& testCases)
-        {
-          testCases.push_back({ "Increasing",{ 5, 10, 20, 500 },{ 0, 1, 2, 3 } });
-          testCases.push_back({ "NonIncreasing",{ 50, 10 },{ 1 } });
-          testCases.push_back({ "Trivial",{ -97864 },{ 0 } });
-          testCases.push_back({ "Many",{ -50, 10, 90, 25, -3, 1000, 50, 500 },{ 0, 1, 3, 6, 7 } });
-        }
-
-        LongestIncreasingSubsequenceTest::TestCase::TestCase(
-          string&& name, TContainer&& data, Indexes&& expected)
-          : BaseTestCase(forward<string>(name)),
-          _Data(forward<TContainer>(data)), _Expected(forward<Indexes>(expected))
-        {
-        }
-
-        void LongestIncreasingSubsequenceTest::TestCase::Print(ostream& str) const
-        {
-          BaseTestCase::Print(str);
-
-          AppendSeparator(str);
-          PrintVectorUtilities::Print("Data", _Data, str);
-          PrintVectorUtilities::Print("Expected", _Expected, str);
-        }
-
-        void LongestIncreasingSubsequenceTest::RunTestCase(const TestCase& testCase)
-        {
-          using TComparer = less<Number>;
-          using Alg = Indexes(*)(const TContainer& source, TComparer);
-          using NameAlg = pair<string, Alg>;
-
-          const vector<NameAlg> subTests{
-            { "Slow",LongestIncreasingSubsequence<Number>::Slow },
-            { "Fast",LongestIncreasingSubsequence<Number>::Fast },
-          };
-
-          const string separator = "_";
-          for (const auto& subTest : subTests)
-          {
-            const string name = testCase.get_Name() + separator + subTest.first;
-            const auto actual = subTest.second(testCase.get_Data(), TComparer());
-            Assert::AreEqual(testCase.get_Expected(), actual, name);
-          }
-        }
-
-        void LongestIncreasingSubsequenceTest::Test()
-        {
-          TestUtilities<TestCase>::Test(RunTestCase, GenerateTestCases);
-        }
-
-        void LongestIncreasingSubsequenceTests(void)
-        {
-          LongestIncreasingSubsequenceTest::Test();
-        }
-      }
+    const string separator = "_";
+    for (const auto& subTest : subTests)
+    {
+      const string name = testCase.get_Name() + separator + subTest.first;
+      const auto actual = subTest.second(testCase.get_Data(), TComparer());
+      Assert::AreEqual(testCase.get_Expected(), actual, name);
     }
   }
+}
+
+void MyCompany::Algorithms::Numbers::Tests::LongestIncreasingSubsequenceTests()
+{
+  TestUtilities<TestCase>::Test(RunTestCase, GenerateTestCases);
 }
